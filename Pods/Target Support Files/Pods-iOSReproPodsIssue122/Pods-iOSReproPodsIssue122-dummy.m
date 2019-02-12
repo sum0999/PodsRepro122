9ee9525a1dd8163241997fed1327f07408256ea9
App Center <vsac@ManigandansiMac.guest.corp.microsoft.com>
2019-02-12 20:56:15 +0530

    Added pods

---
+++ Pods/Target Support Files/Pods-iOSReproPodsIssue122/Pods-iOSReproPodsIssue122-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_iOSReproPodsIssue122 : NSObject
+@end
+@implementation PodsDummy_Pods_iOSReproPodsIssue122
+@end
---
+++ Pods/Target Support Files/Pods-iOSReproPodsIssue122UITests/Pods-iOSReproPodsIssue122UITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_iOSReproPodsIssue122UITests : NSObject
+@end
+@implementation PodsDummy_Pods_iOSReproPodsIssue122UITests
+@end
