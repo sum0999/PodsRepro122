9403ea1bca885af6e3774222e93583f0c0846a04
App Center <vsac@manigandans-imac.local>
2019-02-12 19:35:59 +0530

    Initial Commit

---
+++ iOSReproPodsIssue122/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iOSReproPodsIssue122
+//
+//  Created by App Center on 12/02/19.
+//  Copyright © 2019 App Center. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ iOSReproPodsIssue122Tests/iOSReproPodsIssue122Tests.swift
@@ -0,0 +1,34 @@
+//
+//  iOSReproPodsIssue122Tests.swift
+//  iOSReproPodsIssue122Tests
+//
+//  Created by App Center on 12/02/19.
+//  Copyright © 2019 App Center. All rights reserved.
+//
+
+import XCTest
+@testable import iOSReproPodsIssue122
+
+class iOSReproPodsIssue122Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
